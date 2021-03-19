2f6e1abb67faa6520aad880b0a4379c716e3b9c5
marcospereira <marcosenpereira@gmail.com>
2021-03-19 20:51:07 -0300

    Add Lesson, Seed lesson

--- backend/src/main/java/com/bootcampds2/dslearnbds/entities/Enrollment.java
+++ backend/src/main/java/com/bootcampds2/dslearnbds/entities/Enrollment.java
@@ -2,10 +2,13 @@ package com.bootcampds2.dslearnbds.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import com.bootcampds2.dslearnbds.entities.pk.EnrollmentPK;
@@ -23,6 +26,9 @@ public class Enrollment implements Serializable{
 	private Instant refundMoment;
 	private boolean available;
 	private boolean onlyUpdate;
+	
+	@ManyToMany(mappedBy = "enrollmentsDone")
+	private Set<Lesson> lessonsDone = new HashSet<>();
 	
 	public Enrollment() {
 		
@@ -86,9 +92,8 @@ public class Enrollment implements Serializable{
 		this.onlyUpdate = onlyUpdate;
 	}
 
-	public static long getSerialversionuid() {
-		return serialVersionUID;
+	public Set<Lesson> getLessonsDone() {
+		return lessonsDone;
 	}
-	
 	
 }
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -26,4 +26,18 @@ INSERT INTO tb_section(title, description, position, img_Uri, resource_id, prere
 INSERT INTO tb_section(title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capitulo 2', 'Capitulo 2', 2, 'https://cdn.pixabay.com/photo/2018/03/22/10/55/training-course-3250007_960_720.jpg', 1, 1);
 INSERT INTO tb_section(title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capitulo 3', 'Capitulo 3', 3, 'https://cdn.pixabay.com/photo/2018/03/22/10/55/training-course-3250007_960_720.jpg', 1, 2);
 
+INSERT INTO tb_enrollment(user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (1,1, TIMESTAMP WITH TIME ZONE '2020-07-13T17:50:07.12345Z', null, true, false);
+INSERT INTO tb_enrollment(user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (2,1, TIMESTAMP WITH TIME ZONE '2020-07-13T17:50:07.12345Z', null, true, false);
+
+INSERT INTO tb_lesson(title, position, section_id) VALUES ('Aula 1 do Capitulo 1', 1, 1);
+INSERT INTO tb_content(id, text_Content, video_Uri) VALUES (1, 'Material de Apoio: ABC', 'https://www.youtube.com/watch?v=c64a76Fen5U');
+INSERT INTO tb_lesson(title, position, section_id) VALUES ('Aula 2 do Capitulo 1', 2, 1);
+INSERT INTO tb_content(id, text_Content, video_Uri) VALUES (2, 'Material de Apoio: CDF', 'https://www.youtube.com/watch?v=c64a76Fen5U');
+INSERT INTO tb_lesson(title, position, section_id) VALUES ('Aula 3 do Capitulo 1', 3, 1);
+INSERT INTO tb_content(id, text_Content, video_Uri) VALUES (3, 'Material de Apoio: EFG', 'https://www.youtube.com/watch?v=c64a76Fen5U');
+INSERT INTO tb_lesson(title, position, section_id) VALUES ('Aula 3 do Capitulo 1', 4, 1);
+INSERT INTO tb_task(id, description, question_Count, approval_Count, weigth, due_Date) VALUES (4, 'Fazer Trabalho',5, 4, 1.0, TIMESTAMP WITH TIME ZONE '2020-07-31T20:50:07.12345Z');
+
+INSERT INTO tb_lessons_done(lesson_id, user_id, offer_id) VALUES (1, 1, 1);
+INSERT INTO tb_lessons_done(lesson_id, user_id, offer_id) VALUES (2, 1, 1);
 
